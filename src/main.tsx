d47cf5bf17d8343e2c10d6170db5d9b8a73a58f5
SeedONova <putubaguswidia@gmail.com>
2025-06-16 20:53:42 +0700

    feat: lazy loading

--- src/main.tsx
+++ src/main.tsx
@@ -1,38 +1,26 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
-import { StrictMode } from "react";
+import { StrictMode, lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import App from "./App.tsx";
-import BusinessUnit from "./pages/BusinessUnit.tsx";
-import ComingSoon from "./pages/ComingSoon.tsx";
-import Admin from "./pages/admin/Admin.tsx";
-import Login from "./pages/Login.tsx";
+
+const App = lazy(() => import("./App.tsx"));
+const Login = lazy(() => import("./pages/Login.tsx"));
+const BusinessUnit = lazy(() => import("./pages/BusinessUnit.tsx"));
+const ComingSoon = lazy(() => import("./pages/ComingSoon.tsx"));
+const Admin = lazy(() => import("./pages/admin/Admin.tsx"));
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <App />,
-	},
-	{
-		path: "/login",
-		element: <Login />,
-	},
-	{
-		path: "/?business-unit/:bu",
-		element: <BusinessUnit />,
-	},
-	{
-		path: "/coming-soon",
-		element: <ComingSoon />,
-	},
-	{
-		path: "/admin",
-		element: <Admin />,
-	},
+  { path: "/", element: <App /> },
+  { path: "/login", element: <Login /> },
+  { path: "/business-unit/:bu", element: <BusinessUnit /> },
+  { path: "/coming-soon", element: <ComingSoon /> },
+  { path: "/admin", element: <Admin /> },
 ]);
 
 createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<RouterProvider router={router} />
-	</StrictMode>
+  <StrictMode>
+    <Suspense fallback={<div>Loading…</div>}>
+      <RouterProvider router={router} />
+    </Suspense>
+  </StrictMode>
 );
