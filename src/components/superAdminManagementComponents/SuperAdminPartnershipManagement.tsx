4171f759793e2ee08d8e5818acb83d3493d2c589
Fozzzzy <andrewtedj@gmail.com>
2025-07-10 18:15:21 +0700

    fix: partnership slicing conditional bug

--- src/components/superAdminManagementComponents/SuperAdminPartnershipManagement.tsx
+++ src/components/superAdminManagementComponents/SuperAdminPartnershipManagement.tsx
@@ -150,193 +150,174 @@ export default function SuperAdminPartnershipManagement() {
 		}
 	}
 
-	const userRole = localStorage.getItem("userRole");
-
 	return (
 		<>
-			{userRole == "admin" ? (
-				<>
-					<section className="bg-black border border-[#404040] p-[20px] rounded-[12px] space-y-[24px]">
-						<section className="flex justify-between items-center">
-							<h1 className="text-lg lg:text-2xl font-semibold">
-								Partnership Management Panel
-							</h1>
-							<button
-								onClick={() => setPartnershipPopUp(true)}
-								className="cursor-pointer bg-primary text-sm lg:text-base p-[16px] rounded-[8px]"
-							>
-								Add Partner +
-							</button>
-						</section>
-						{partnershipData && (
-							<section className="grid grid-cols-12 gap-x-[20px]">
-								<div className="col-span-12 flex items-center bg-neutral-800 gap-[10px] p-[12px] rounded-[8px]">
-									<Search />
-									<input
-										className="w-full h-full text-white placeholder-white outline-none"
-										type="text"
-										placeholder="Sarch"
-									/>
-								</div>
-							</section>
-						)}
-					</section>
-					<section className="overflow-scroll xl:overflow-auto h-[500px] bg-black flex flex-col justify-start border border-[#404040] rounded-[20px] p-4">
-						<table className="table-auto w-[700px] xl:w-full h-fit text-white">
-							<thead>
-								<tr className="border-b border-[#D4D4D4] text-left">
-									<th className="text-base lg:text-lg font-bold py-3 px-4">
-										No
-									</th>
-									<th className="text-base lg:text-lg font-bold py-3 px-4">
-										Displayed
-									</th>
-									<th className="text-base lg:text-lg font-bold py-3 px-20">
-										Logo
-									</th>
-									<th className="text-base lg:text-lg font-bold py-3 px-4">
-										Name
-									</th>
-									<th className="text-base lg:text-lg font-bold py-3 px-1">
-										Actions
-									</th>
-								</tr>
-							</thead>
-							<tbody className="relative">
-								{partnershipData ? (
-									partnershipData.map((data, index) => (
-										<tr
-											key={index}
-											className="border-b border-[#D4D4D4]"
-										>
-											<td className="py-4 px-4 align-top text-sm font-medium">
-												{index + 1}
-											</td>
-											<td className="py-4 px-4 align-top text-sm font-medium">
-												<input
-													onChange={() =>
-														handleCheck(index)
-													}
-													checked={
-														data.checked || false
-													}
-													className="cursor-pointer w-[40px] h-[40px] appearance-none border-2 border-white bg-transparent rounded-md checked:bg-transparent checked:border-white relative before:content-[''] before:absolute before:top-4 before:left-1/2 before:transform before:-translate-x-1/2 before:-translate-y-1/2 before:w-4 before:h-2 before:border-l-2 before:border-b-2 before:border-white before:rotate-[-45deg] before:opacity-0 checked:before:opacity-100"
-													type="checkbox"
-												/>
-											</td>
-											<td className="py-4 px-20 align-top">
-												<img
-													className="w-[50px] h-[50px]"
-													src={data.image}
-													alt=""
-												/>
-											</td>
-											<td className="py-4 px-4 align-top text-base font-normal whitespace-nowrap">
-												{data.partnerName}
-											</td>
-											<td className="py-4 align-top">
-												<div className="flex items-center gap-x-[16px]">
-													<div
-														onClick={() =>
-															showDetail(index)
-														}
-														className="cursor-pointer w-[34px] h-[34px] flex justify-center items-center border border-[#FF8800] p-[8px] rounded-[8px]"
-													>
-														<PencilIcon
-															width={18}
-															height={18}
-															color="#FF8800"
-														/>
-													</div>
-													<div
-														onClick={() =>
-															handleDeletePopUp(
-																index
-															)
-														}
-														className="cursor-pointer w-[36px] h-[36px] flex justify-center items-center border border-[#C00707] rounded-md"
-													>
-														<TrashCanIcon
-															width={16}
-															height={18}
-															color="#C00707"
-														/>
-													</div>
-												</div>
-											</td>
-										</tr>
-									))
-								) : (
-									<tr className="h-[250px]">
-										<td colSpan={5}>
-											<div className="flex flex-col justify-center items-center text-white">
-												<h1 className="text-xl lg:text-3xl font-black">
-													NO DATA
-												</h1>
-											</div>
-										</td>
-									</tr>
-								)}
-							</tbody>
-						</table>
-						<div
-							className={`mt-3 w-[1000px] xl:w-full ${
-								partnershipData && partnershipData.length > 0
-									? "flex"
-									: "hidden"
-							} ${
-								partnershipData &&
-								partnershipData.length > 0 &&
-								partnershipData?.filter(
-									(item) => item.checked == true
-								).length < 3
-									? "justify-between"
-									: "justify-end"
-							} items-center`}
-						>
-							{partnershipData &&
-								partnershipData.length > 0 &&
-								partnershipData?.filter(
-									(item) => item.checked == true
-								).length < 3 && (
-									<div className="flex items-center text-[#FB923C] gap-x-[10px]">
-										<TriangleAlert />
-										<p>
-											Please select minimum 3 partners to
-											display.
-										</p>
-									</div>
-								)}
-							<div className="flex justify-end items-center gap-2">
-								<button className="h-full bg-[#1c1c1c] text-white px-3 py-1 rounded-md border border-white/20">
-									<LeftChevronIcon
-										width={23}
-										height={23}
-										color="white"
-									/>
-								</button>
-								<div className="bg-[#1c1c1c] text-white px-3 py-1 rounded-md border border-white/20 appearance-none">
-									<select className="bg-none p-0">
-										<option className="bg-none p-0">
-											1
-										</option>
-									</select>
-								</div>
-								<p className="text-white">of 1</p>
-								<button className="h-full bg-[#1c1c1c] text-white px-3 py-1 rounded-md border border-white/20 rotate-180">
-									<LeftChevronIcon
-										width={23}
-										height={23}
-										color="white"
-									/>
-								</button>
-							</div>
+			<section className="bg-black border border-[#404040] p-[20px] rounded-[12px] space-y-[24px]">
+				<section className="flex justify-between items-center">
+					<h1 className="text-lg lg:text-2xl font-semibold">
+						Partnership Management Panel
+					</h1>
+					<button
+						onClick={() => setPartnershipPopUp(true)}
+						className="cursor-pointer bg-primary text-sm lg:text-base p-[16px] rounded-[8px]"
+					>
+						Add Partner +
+					</button>
+				</section>
+				{partnershipData && (
+					<section className="grid grid-cols-12 gap-x-[20px]">
+						<div className="col-span-12 flex items-center bg-neutral-800 gap-[10px] p-[12px] rounded-[8px]">
+							<Search />
+							<input
+								className="w-full h-full text-white placeholder-white outline-none"
+								type="text"
+								placeholder="Sarch"
+							/>
 						</div>
 					</section>
-				</>
-			) : (
-				<SuperAdminPartnershipManagement />
-			)}
+				)}
+			</section>
+			<section className="overflow-scroll xl:overflow-auto h-[500px] bg-black flex flex-col justify-start border border-[#404040] rounded-[20px] p-4">
+				<table className="table-auto w-[700px] xl:w-full h-fit text-white">
+					<thead>
+						<tr className="border-b border-[#D4D4D4] text-left">
+							<th className="text-base lg:text-lg font-bold py-3 px-4">
+								No
+							</th>
+							<th className="text-base lg:text-lg font-bold py-3 px-4">
+								Displayed
+							</th>
+							<th className="text-base lg:text-lg font-bold py-3 px-20">
+								Logo
+							</th>
+							<th className="text-base lg:text-lg font-bold py-3 px-4">
+								Partner
+							</th>
+							<th className="text-base lg:text-lg font-bold py-3 px-1">
+								Actions
+							</th>
+						</tr>
+					</thead>
+					<tbody className="relative">
+						{partnershipData ? (
+							partnershipData.map((data, index) => (
+								<tr
+									key={index}
+									className="border-b border-[#D4D4D4]"
+								>
+									<td className="py-4 px-4 align-top text-sm font-medium">
+										{index + 1}
+									</td>
+									<td className="py-4 px-4 align-top text-sm font-medium">
+										<input
+											onChange={() => handleCheck(index)}
+											checked={data.checked || false}
+											className="cursor-pointer w-[40px] h-[40px] appearance-none border-2 border-white bg-transparent rounded-md checked:bg-transparent checked:border-white relative before:content-[''] before:absolute before:top-4 before:left-1/2 before:transform before:-translate-x-1/2 before:-translate-y-1/2 before:w-4 before:h-2 before:border-l-2 before:border-b-2 before:border-white before:rotate-[-45deg] before:opacity-0 checked:before:opacity-100"
+											type="checkbox"
+										/>
+									</td>
+									<td className="py-4 px-20 align-top">
+										<img
+											className="w-[50px] h-[50px]"
+											src={data.image}
+											alt=""
+										/>
+									</td>
+									<td className="py-4 px-4 align-top text-base font-normal whitespace-nowrap">
+										{data.partnerName}
+									</td>
+									<td className="py-4 align-top">
+										<div className="flex items-center gap-x-[16px]">
+											<div
+												onClick={() =>
+													showDetail(index)
+												}
+												className="cursor-pointer w-[34px] h-[34px] flex justify-center items-center border border-[#FF8800] p-[8px] rounded-[8px]"
+											>
+												<PencilIcon
+													width={18}
+													height={18}
+													color="#FF8800"
+												/>
+											</div>
+											<div
+												onClick={() =>
+													handleDeletePopUp(index)
+												}
+												className="cursor-pointer w-[36px] h-[36px] flex justify-center items-center border border-[#C00707] rounded-md"
+											>
+												<TrashCanIcon
+													width={16}
+													height={18}
+													color="#C00707"
+												/>
+											</div>
+										</div>
+									</td>
+								</tr>
+							))
+						) : (
+							<tr className="h-[250px]">
+								<td colSpan={5}>
+									<div className="flex flex-col justify-center items-center text-white">
+										<h1 className="text-xl lg:text-3xl font-black">
+											NO DATA
+										</h1>
+									</div>
+								</td>
+							</tr>
+						)}
+					</tbody>
+				</table>
+				<div
+					className={`mt-3 w-[1000px] xl:w-full ${
+						partnershipData && partnershipData.length > 0
+							? "flex"
+							: "hidden"
+					} ${
+						partnershipData &&
+						partnershipData.length > 0 &&
+						partnershipData?.filter((item) => item.checked == true)
+							.length < 3
+							? "justify-between"
+							: "justify-end"
+					} items-center`}
+				>
+					{partnershipData &&
+						partnershipData.length > 0 &&
+						partnershipData?.filter((item) => item.checked == true)
+							.length < 3 && (
+							<div className="flex items-center text-[#FB923C] gap-x-[10px]">
+								<TriangleAlert />
+								<p>
+									Please select minimum 3 partners to display.
+								</p>
+							</div>
+						)}
+					<div className="flex justify-end items-center gap-2">
+						<button className="h-full bg-[#1c1c1c] text-white px-3 py-1 rounded-md border border-white/20">
+							<LeftChevronIcon
+								width={23}
+								height={23}
+								color="white"
+							/>
+						</button>
+						<div className="bg-[#1c1c1c] text-white px-3 py-1 rounded-md border border-white/20 appearance-none">
+							<select className="bg-none p-0">
+								<option className="bg-none p-0">1</option>
+							</select>
+						</div>
+						<p className="text-white">of 1</p>
+						<button className="h-full bg-[#1c1c1c] text-white px-3 py-1 rounded-md border border-white/20 rotate-180">
+							<LeftChevronIcon
+								width={23}
+								height={23}
+								color="white"
+							/>
+						</button>
+					</div>
+				</div>
+			</section>
 
 			<SuperAdminPartnershipPopUp
 				open={partnershipPopUp}
